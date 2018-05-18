5c01b12f23da536df259e1d0963950082f03e63b
Douglas Craig <dcraig112@gmail.com>
2018-05-18 22:34:49 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,9 +2,9 @@ def fizzbuzz(n)
     if n % 3 == 0 && n % 5 != 0
       "Fizz"
     elsif n % 3 != 0 && n % 5 == 0
-      puts "Buzz"
+      "Buzz"
     elsif n % 3 == 0 && n % 5 == 0
-      puts "FizzBuzz"
+      "FizzBuzz"
     else
       nil
     end
